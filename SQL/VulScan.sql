1bffbdd4847701d021bbe788084ec05cde87a364
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-11-02 13:38:34 -0400

    vulscan db table template

--- SQL/VulScan.sql
+++ SQL/VulScan.sql
@@ -1,6 +1,20 @@
 -- Create Table to hold VulScan Data
 CREATE TABLE vulscan_data (
-    
+    id VARCHAR,
+    ip VARCHAR,
+    host VARCHAR,
+    os VARCHAR,
+    proto VARCHAR,
+    port VARCHAR,
+    service VARCHAR,
+    product VARCHAR,
+    service_fp VARCHAR,
+    nse_script_id VARCHAR,
+    notes VARCHAR,
+    cve_id VARCHAR,
+    cve_title VARCHAR,
+    cve_link VARCHAR,
+    matches VARCHAR,
 );
 
 -- Import Data form csv into DB
