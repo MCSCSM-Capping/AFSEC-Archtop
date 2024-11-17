f97112ad5608c681c31260117ca5ee6c3a1c0b21
blitz punch <andrew-schmitter@hotmail.com>
2024-11-17 16:31:52 -0500

    Fetching the main table data into data object

---
+++ AFSEC-Dashboard-main/src/data/dataInterface.ts
@@ -0,0 +1,6 @@
+export interface dataInterface {
+    scanner: string
+    scan_source: string
+    scan_date: string
+    scan_info: string
+  }
--- AFSEC-Dashboard-main/src/data/scannerData.ts
+++ AFSEC-Dashboard-main/src/data/scannerData.ts
@@ -1,6 +1,28 @@
 import { GridRowsProp } from '@mui/x-data-grid';
 // import { formatNumber } from 'functions/formatNumber';
+import type { dataInterface } from './dataInterface'
 
+export async function getData(): Promise<dataInterface[]> {
+  const request = await fetch('http://localhost:3001/api/main_table')
+  const data = await request.json()
+  return data
+}
+
+// testing
+(async () => {
+  try {
+    const data = await getData();
+    console.log('Fetched data:', data);
+
+    if (data.length > 0) {
+      console.log('Data is holding:', data);
+    } else {
+      console.log('No data found.');
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+})();
 
 // Scan data
 export const scannerData: GridRowsProp = [
@@ -10,92 +32,4 @@ export const scannerData: GridRowsProp = [
     date: new Date('Jan 30, 2024'),
     scanInfo: 'The Cisco Small Business 200 Series Smart Switch 1.2.7.76 and earlier, Small Business 300 Series Managed Switch 1.2.7.76 and earlier, and Small Business 500 Series Stackable Managed Switch 1.2.7.76 and earlier allow remote attackers to cause a denial of service (SSL/TLS layer outage) via malformed (1) SSH or (2) SSL packets, aka Bug ID CSCua30246. - Link:  (5)',
   },
-  /*{
-    id: 'Vulscan',
-    cve: { name: 'Sophie Moore', description: 'contact@sophiemoore.com' },
-    date: new Date('Jan 27, 2024'),
-    status: 'canceled',
-    country: 'Kingston',
-    total: formatNumber(5870.32),
-  },
-  {
-    id: 'Vulscan',
-    cve: { name: 'Matt Cannon', description: 'info@mattcannon.com' },
-    date: new Date('Jan 24, 2024'),
-    status: 'delivered',
-    country: 'Warwick',
-    total: formatNumber(13899.48),
-  },
-  {
-    id: 'SpiderFoot',
-    cve: { name: 'Graham Hills', description: 'hi@grahamhills.com' },
-    date: new Date('Jan 21, 2024'),
-    status: 'pending',
-    country: 'Warwick',
-    total: formatNumber(1569.12),
-  },
-  {
-    id: '#1528',
-    cve: { name: 'Sandy Houston', description: 'contact@sandyhouston.com' },
-    date: new Date('Jan 18, 2024'),
-    status: 'delivered',
-    country: 'Hancock',
-    total: formatNumber(899.16),
-  },
-  {
-    id: '#1527',
-    cve: { name: 'Andy Smith', description: 'hello@andysmith.com' },
-    date: new Date('Jan 15, 2024'),
-    status: 'pending',
-    country: 'Germantown',
-    total: formatNumber(2449.64),
-  },
-  {
-    id: '#1526',
-    cve: { name: 'Emma Grace', description: 'wow@emmagrace.com' },
-    date: new Date('Jan 12, 2024'),
-    status: 'delivered',
-    country: 'Germantown',
-    total: formatNumber(6729.82),
-  },
-  /*{
-    id: '#1525',
-    cve: { name: 'Ava Rose', description: 'me@avarose.com' },
-    date: new Date('Jan 09, 2024'),
-    status: 'canceled',
-    country: 'Kingston',
-    total: formatNumber(784.94),
-  },
-  {
-    id: '#1524',
-    cve: { name: 'Olivia Jane', description: 'info@oliviajane.com' },
-    date: new Date('Jan 06, 2024'),
-    status: 'pending',
-    country: 'Kingston',
-    total: formatNumber(1247.86),
-  },
-  {
-    id: '#1523',
-    cve: { name: 'Mason Alexander', description: 'myinfo@alexander.com' },
-    date: new Date('Jan 03, 2024'),
-    status: 'delivered',
-    country: 'Warwick',
-    total: formatNumber(304.89),
-  },
-  {
-    id: '#1522',
-    cve: { name: 'Samuel David', description: 'me@samueldavid.com' },
-    date: new Date('Jan 01, 2024'),
-    status: 'pending',
-    country: 'Hancock',
-    total: formatNumber(2209.76),
-  },
-  {
-    id: '#1521',
-    cve: { name: 'Henry Joseph', description: 'contact@henryjoseph.com' },
-    date: new Date('Dec 28, 2023'),
-    status: 'delivered',
-    country: 'Warwick',
-    total: formatNumber(5245.68),
-  },*/
 ];
