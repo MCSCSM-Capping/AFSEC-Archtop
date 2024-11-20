a1b2363ea17fa7775a65d2400ceff2f1a2f3db17
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-11-20 11:57:18 -0500

    11/20/24 idea did not work caused hook errors have both there just in case

--- AFSEC-Dashboard-main/src/data/scannerData.ts
+++ AFSEC-Dashboard-main/src/data/scannerData.ts
@@ -56,13 +56,24 @@ or move all this stuff to the old file and add it over there not sure what the b
 */
 
 //what type is data
-//what type is data
-function data2Grid(data: dataInterface[]){
+//one that matches the format of the table but cause a wild amount of hook errors and such
+/*function data2Grid(data: dataInterface[]){
   return data.map((item) => ({
     id: item.scanner,
     scanSource: item.scan_source,
     date: item.scan_date,
     scanInfo: item.scan_info,
+   }));
+}*/
+
+//no hook error but does not load it in to the dashboard
+function data2Grid(data: dataInterface[]){
+  return data.map((item, index) => ({
+    id: index + 1,
+    scanner: item.scanner,
+    scan_source: item.scan_source,
+    scan_date: item.scan_date,
+    scan_info: item.scan_info,
    }));
 }
 
