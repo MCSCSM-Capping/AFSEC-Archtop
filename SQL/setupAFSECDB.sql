14183436dbd8a73c3be864dcd32ae4c6db5b64c0
RyanRosenkranse <Ryan.Rosenkranse1@marist.edu>
2024-11-23 18:48:37 -0500

    created setup scripts

---
+++ SQL/setupAFSECDB.sql
@@ -0,0 +1,44 @@
+-- Switch to user Postgres
+-- Start postgres with psql
+
+-- create the DB
+CREATE DATABASE afsec;
+
+-- connect to database afsec
+-- \c afsec
+
+-- create tables
+
+-- Create Table to hold Spiderfoot Data
+CREATE TABLE spiderfoot_data (
+    generated VARCHAR,
+    type VARCHAR,
+    data VARCHAR,
+    source_ip VARCHAR,
+    scan_date DATE
+);
+
+-- Create Table to hold VulScan Data
+CREATE TABLE vulscan_data (
+    ip VARCHAR,
+    host VARCHAR,
+    os VARCHAR,
+    protocol VARCHAR,
+    port VARCHAR,
+    service VARCHAR,
+    product VARCHAR,
+    id VARCHAR,
+    title VARCHAR,
+    scan_date DATE
+);
+
+-- Create Table to hold Spiderfoot Data
+CREATE TABLE main_table (
+    Scanner VARCHAR, -- either vulscan or spiderfoot 
+    Scan_Source VARCHAR, -- spiderfoot: source column | vulscan: ip + port
+    Scan_Date VARCHAR, -- spiderfoot: updated column | vulscan: scan_date
+    Scan_Info VARCHAR, -- spiderfoot: type + module + fp + data | vulscan: host + os + protocol + service + product + id + title
+
+);
+
+
---
+++ Scripts/setup.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+# Update Packages
+sudo apt update
+
+# Install Docker + Start
+sudo apt install docker.io
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Confirm that its working properly
+docker --version
+
+# Pull Spiderfoot Container
+sudo docker pull josaorg/spiderfoot
+
+# Start spiderfoot container
+sudo docker run --name spiderfoot -d -p 5001:5001 josaorg/spiderfoot
+
+# Install nmap + Vulscan
+
+sudo apt install nmap
+
+nmap --version
+
+cd /usr/share/nmap/scripts/
+sudo git clone https://github.com/scipag/vulscan.git
+
+# Install Postgres + Check Status
+sudo apt install postgresql postgresql-contrib
+
+sudo systemctl status postgresql
+
+sudo cd ~
+
+sudo git clone https://github.com/MCSCSM-Capping/AFSEC-Archtop.git
+
+echo "Initial Setup Script Complete"
+echo "Please proceed to Postgres DB setup"
+
