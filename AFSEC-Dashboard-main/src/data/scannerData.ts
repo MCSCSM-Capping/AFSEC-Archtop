97fee4ee2550995d4605452250da0dfb2c286fbd
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-11-26 20:33:05 -0500

    dynamic api and db calls done. Set to every 5 mins currently

--- AFSEC-Dashboard-main/src/data/scannerData.ts
+++ AFSEC-Dashboard-main/src/data/scannerData.ts
@@ -1,17 +1,12 @@
 import { GridRowsProp } from '@mui/x-data-grid';
 import type { dataInterface } from './dataInterface'
 
-// grabs json from api
-export async function getData(): Promise<dataInterface[]> {
-  const request = await fetch('http://localhost:3001/api/main_table')
-  const data = await request.json()
-  return data
-}
-
 export let scannerData: GridRowsProp = [];
+apiCall();  // initial api call
+setInterval(apiCall, 300000); // interval to dynamically call the api every 5 minutes
 
 // calls getData and data2Grid
-(async () => {
+async function apiCall(){
   try {
     const data = await getData();
     console.log('Fetched data:', data);
@@ -26,7 +21,14 @@ export let scannerData: GridRowsProp = [];
   } catch (error) {
     console.error('Error fetching data:', error);
   }
-})();
+}
+
+// grabs json from api
+export async function getData(): Promise<dataInterface[]> {
+  const request = await fetch('http://localhost:3001/api/main_table')
+  const data = await request.json()
+  return data
+}
 
 // maps data from json to table format
 function data2Grid(data: dataInterface[]){
@@ -34,7 +36,18 @@ function data2Grid(data: dataInterface[]){
     id: index + 1,
     scanner: item.scanner,
     scan_source: item.scan_source,
-    scan_date: new Date(item.scan_date),
+    scan_date: new Date('Jan 30,2024'),
     scan_info: item.scan_info,
+
+    // below is not added in yet but these should be ideally mapped as well
+    /*
+    scan_ip: string
+    scan_port: string
+    scan_protocol: string
+    scan_service: string
+    scan_product: string
+    scan_cve_id: string
+    scan_title: string
+    */
    }));
 }
--- pg-api/server.js
+++ pg-api/server.js
@@ -1,7 +1,7 @@
 // This Express app will make use of 'dotenv' and 'pg' libraries
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
 const cors = require('cors');
 
@@ -11,7 +11,7 @@ require('dotenv').config();  // loads environment variables from .env file
 
 const { Pool } = require('pg');
 
-// Create a new pool instance with database connection configuration
+// DB connection pool
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -19,37 +19,46 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
-
-// Test route
-// Navigate to http://localhost:3001 to verify the Express server is running
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-  })
  
-// Test route
-// Navigate to http://localhost:3001/test-db in the browser to test connection to the DB
-app.get('/test-db', async (req, res) => {
-    try {
-      const result = await pool.query('SELECT NOW()');
-      res.json({ message: "Database connected successfully!", data: result.rows });
-    } catch (error) {
-      console.error('Database connection error:', error);
-      res.status(500).json({ message: 'Database connection failed', error });
-    }
-  });
+let cache = [];  // cache of all the current data
+setInterval(fetchData, 5000); // dynamic database calls --day 86400000
+fetchData(); // calls this function when the server start
 
-app.get('/api/main_table', async (req, res) => {
+// function to dynamically grab the distinct records from the main_table
+async function fetchData(){
   try {
-    const result = await pool.query('SELECT * FROM main_table');
-    res.json(result.rows);  // Send all rows as JSON response
+    const result = await pool.query('SELECT DISTINCT * FROM main_table');
+    cache = result.rows;
   } catch (error) {
     console.error('Error fetching data from main_table:', error);
     res.status(500).json({ message: 'Error fetching data' });
   }
+}
+
+// give the front end the cache when it reqs it
+app.get('/api/main_table', (req, res) => {
+  res.json(cache);  // Send all rows as JSON response
 });
 
-  
+// start the api server
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+    console.log(`App running on port ${port}.`);
+});
 
+// test db connection
+// Navigate to http://localhost:3001/test-db in the browser to test connection to the DB
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: "Database connected successfully!", data: result.rows });
+  } catch (error) {
+    console.error('Database connection error:', error);
+    res.status(500).json({ message: 'Database connection failed', error });
+  }
+});
+
+// test api
+// Navigate to http://localhost:3001 to verify the Express server is running
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
