2ae5cb9a35634b3299b266e841d21a3650f0165d
SorinMac <97994615+SorinMac@users.noreply.github.com>
2024-11-27 10:54:12 -0500

    11/27/24 testing the parsing code on the server now

--- AFSEC-Dashboard-main/src/data/scannerData.ts
+++ AFSEC-Dashboard-main/src/data/scannerData.ts
@@ -23,6 +23,26 @@ async function apiCall(){
   }
 }
 
+async function parsing(index:number, data:string): Promise<string> {
+  let parse = "";
+  const regex = /:/g;
+
+  let instance = index; 
+  let counter = 0; 
+
+  for (const match of data.matchAll(regex)) {
+      counter++;
+      if (counter === instance) {
+          const start = match.index + 1; 
+          const end = data.indexOf(":", start); 
+          parse = data.substring(start, end); 
+          break; 
+      }
+  }
+
+  return parse
+}
+
 // grabs json from api
 export async function getData(): Promise<dataInterface[]> {
   const request = await fetch('http://localhost:3001/api/main_table')
@@ -38,6 +58,7 @@ function data2Grid(data: dataInterface[]){
     scan_source: item.scan_source,
     scan_date: new Date(item.scan_date),
     scan_info: item.scan_info,
+    protocol: parsing(3, item.scan_info),
 
     // below is not added in yet but these should be ideally mapped as well
     /*
