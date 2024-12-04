7f512017ca6103609f3ab07b3913cae5a815ee0b
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-12-04 16:35:06 -0500

    fixed vulscan scan script to accept single, range, or cdir ips

--- vulscan/vulscanscript.sh
+++ vulscan/vulscanscript.sh
@@ -23,8 +23,34 @@ while read -r ip; do
     # define the output file name
     OUTPUT_FILE="$OUTPUT_DIR/${ip}.xml"
 
-    #for each ip run vulscan and output the results to its specific file
-    sudo nmap --script-args='vulscandb=cve.csv,vulscanoutput="ID: {id} - Title: {title} - Link: {link} ({matches})\n"' --script=vulscan/vulscan.nse -sV -p- "$ip" -oX "$OUTPUT_FILE"
+    # for each ip run vulscan and output the results to its specific file
+    # range or subnet
+    if [[ "$ip"=~ / ]]; then
+        IFS='/' read -r start_ip cidr <<< "$ip"  # grab the start ip and the cidr
+        IFS='.' read -r one two three four <<< "$start_ip"  # parse the start ip
+        # check range
+        if [[ "$cidr" == "8" ]]; then
+            # a.0.0.0 - a.255.255.255
+            end_ip="$one.255.255.255"
+        elif [[ "$cidr" == "16" ]]; then
+            # a.b.0.0 - a.b.255.255
+            end_ip="$one.two.255.255"
+        elif [[ "$cidr" == "24" ]]; then
+            # a.b.c.0 - a.b.c.255
+            end_ip="$one.two.three.255"
+        else
+            echo unsupported range...
+        fi
+        range="start_ip-end_ip"
+        sudo nmap --script-args='vulscandb=cve.csv,vulscanoutput="ID: {id} - Title: {title} - Link: {link} ({matches})\n"' --script=vulscan/vulscan.nse -sV -p- "$range" -oX "$OUTPUT_FILE"
+    # range
+    elif [[ "$ip" =~ - ]]; then
+        sudo nmap --script-args='vulscandb=cve.csv,vulscanoutput="ID: {id} - Title: {title} - Link: {link} ({matches})\n"' --script=vulscan/vulscan.nse -sV -p- "$ip" -oX "$OUTPUT_FILE"
+    # single
+    else
+        sudo nmap --script-args='vulscandb=cve.csv,vulscanoutput="ID: {id} - Title: {title} - Link: {link} ({matches})\n"' --script=vulscan/vulscan.nse -sV -p- "$ip" -oX "$OUTPUT_FILE"
+    fi
+
     sudo python3 ../Scripts/xml2csv.py -f "$OUTPUT_FILE" -csv "$OUTPUT_DIR/${ip}.csv"
     sudo rm "$OUTPUT_FILE"
     
